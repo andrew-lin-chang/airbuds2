1c4045cc9b43b0f50115861ffbe88da4858ec4d6
Andrew Chang <andrewchang@utexas.edu>
2024-06-22 11:09:43 -0500

    auth code with PKCE

---
+++ server/authPKCE.js
@@ -0,0 +1,65 @@
+/* Authorization Code Flow with PKCE https://github.com/spotify/web-api-examples/blob/master/get_user_profile/src/authCodeWithPkce.ts */
+
+async function redirectToAuthCodeFlow(req, res, clientId) {
+  const verifier = generateCodeVerifier(128);
+  const challenge = await generateCodeChallenge(verifier);
+  const redirect_uri = process.env.REDIRECT_URI;
+
+  req.session.verifier = verifier;
+
+  const params = new URLSearchParams();
+  params.append("client_id", clientId);
+  params.append("response_type", "code");
+  params.append("redirect_uri", redirect_uri);
+  params.append("scope", "user-read-private user-read-email");
+  params.append("code_challenge_method", "S256");
+  params.append("code_challenge", challenge);
+
+  res.redirect(`https://accounts.spotify.com/authorize?${params.toString()}`);
+}
+
+async function getAccessToken(req, res, clientId, code) {
+  const verifier = req.session.verifier;
+  const redirect_uri = process.env.REDIRECT_URI
+
+  const params = new URLSearchParams();
+  params.append("client_id", clientId);
+  params.append("grant_type", "authorization_code");
+  params.append("code", code);
+  params.append("redirect_uri", redirect_uri);
+  params.append("code_verifier", verifier);
+
+  const result = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: params,
+  });
+
+  const { access_token } = await result.json();
+  return access_token;
+}
+
+// PKCE protocol uses code verifier and code challenge
+function generateCodeVerifier(length) {
+  let text = "";
+  let possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+async function generateCodeChallenge(codeVerifier) {
+  const data = new TextEncoder().encode(codeVerifier);
+  const digest = await crypto.subtle.digest("SHA-256", data);
+  return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=+$/, "");
+}
+
+module.exports = {
+  redirectToAuthCodeFlow,
+  getAccessToken,
+};
--- server/server.js
+++ server/server.js
@@ -1,11 +1,67 @@
 const express = require("express");
-const app = express();
+const session = require("express-session");
+const dotenv = require("dotenv");
+const { redirectToAuthCodeFlow, getAccessToken } = require("./authPKCE");
+const { access } = require("fs");
+
 const PORT = 5000;
+dotenv.config({ path: "../.env" });
+
+const spotify_client_id = process.env.SPOTIFY_CLIENT_ID;
+const spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+const app = express();
+
+app.use(
+  session({
+    secret: spotify_client_secret,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.get("/auth/login", (req, res) => {
+  redirectToAuthCodeFlow(req, res, spotify_client_id);
+});
+
+app.get("/auth/callback", async (req, res) => {
+  const code = req.query.code;
+  if (!code) {
+    return res.status(400).send("No code provided");
+  }
+
+  try {
+    const access_token = await getAccessToken(
+      req,
+      res,
+      spotify_client_id,
+      code
+    );
+    req.session.access_token = access_token;
+    const profile = await fetchProfile(access_token);
+    req.session.username = profile.display_name;
+    res.redirect("/");
+  } catch (error) {
+    console.error("Error during callback handling: ", error);
+    res.status(500).send("Internal server error");
+  }
+});
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  if (req.session.username) {
+    res.send("Hi, " + req.session.username);
+  } else {
+    res.send("Connect Spotify account");
+  }
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server started at http://localhost:${PORT}`);
 });
+
+async function fetchProfile(code) {
+  const result = await fetch("https://api.spotify.com/v1/me", {
+    method: "GET",
+    headers: { Authorization: `Bearer ${code}` },
+  });
+  return await result.json();
+}
