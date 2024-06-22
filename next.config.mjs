731a12e20133d58b062ca075ccc4cdff39a3d842
Andrew Chang <andrewchang@utexas.edu>
2024-06-22 12:50:28 -0500

    fix proxy for nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/auth/:path*",
+        destination: "http://localhost:5000/auth/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
